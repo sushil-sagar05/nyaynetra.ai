38fe82a613ab31442490a4f620ae9609a639d3d2
sushil-sagar05 <sagarsushil1403@gmail.com>
2025-04-30 22:26:55 +0530

    cors fixes

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,60 +1,60 @@
-import express, { Request, Response,NextFunction } from "express";
-const connect = require('./DB/db')
-const dotenv = require("dotenv")
+import express, { Response } from "express";
+const connect = require('./DB/db');
+const dotenv = require("dotenv");
 dotenv.config({
-    path:'.env'
-})
+    path: '.env'
+});
 connect();
-const cors = require('cors')
-const app = express()
-import cookieparser from 'cookie-parser'
+const cors = require('cors');
+const app = express();
+import cookieparser from 'cookie-parser';
 import { CorsOptions } from "cors";
-app.set("trust proxy",1);
 
-const corsOptions:CorsOptions = {
+app.set("trust proxy", 1);
+
+const corsOptions: CorsOptions = {
     origin: (origin, callback) => {
-  
-      const allowedOrigin = process.env.FRONTEND_URL;
+        const allowedOrigin = process.env.FRONTEND_URL;
 
-      if (origin === allowedOrigin || !origin) {
-        callback(null, true);  
-      } else {
-  
-        callback(new Error('Not allowed by CORS'), false);  
-      }
+        if (origin === allowedOrigin || !origin) {
+            callback(null, true);  
+        } else {
+            callback(new Error('Not allowed by CORS'), false);  
+        }
     },
     credentials: true, 
-    methods: ['GET', 'POST', 'OPTIONS','DELETE','PATCH'],
+    methods: ['GET', 'POST', 'OPTIONS', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-  };
-  
-  app.use(cors(corsOptions));
-  
+};
 
-app.use((req,res,next)=>{
-    res.setHeader('Access-Control-Allow-Credentials','true');
-    next()
-})
-  app.use(cors(corsOptions));
-  app.use(cookieparser());
-  app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-import {ScheduledDeletion} from './cron/Schedule-deletion'
+app.use(cors(corsOptions));
+
+app.use(cookieparser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+import { ScheduledDeletion } from './cron/Schedule-deletion';
 import { DeleteDeactivateUser } from "./cron/Schedule-deletion";
 import { ScheduledDeletionforMulter } from "./cron/Schedule-deletion";
+
 ScheduledDeletion();
 DeleteDeactivateUser();
 ScheduledDeletionforMulter();
-app.get('/', (req:Request,res:Response)=>{
+
+app.get('/', ( res: Response) => {
     res.send('Hello World');
 });
-import userRoutes from './routes/user.routes'
-import documentRoutes from './routes/document.routes'
-import settingRoutes from './routes/settings.route'
-app.use('/user',userRoutes);
-app.use('/document',documentRoutes)
-app.use('/settings',settingRoutes)
+
+import userRoutes from './routes/user.routes';
+import documentRoutes from './routes/document.routes';
+import settingRoutes from './routes/settings.route';
+
+app.use('/user', userRoutes);
+app.use('/document', documentRoutes);
+app.use('/settings', settingRoutes);
+
 import errors from "./globalError";
-app.use(errors.MulterError)
-app.use(errors.ApiErrors)
+app.use(errors.MulterError);
+app.use(errors.ApiErrors);
+
 module.exports = app;
